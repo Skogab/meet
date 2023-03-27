d8c8df892077a32076be49d832fd7e47faaed817
Skogab <114601288+Skogab@users.noreply.github.com>
2023-03-27 19:17:03 +0200

    Push 4.5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // App.js
 
 import React, { Component } from "react";
+import "./nprogress.css";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
   state = {
@@ -12,15 +14,42 @@ class App extends Component {
     locations: [],
   };
 
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
         <EventList events={this.state.events} />
       </div>
     );
   }
 }
-8;
 
 export default App;
