12c15242ed05ee0481876414a98052602d332916
Skogab <mierau.matthias@googlemail.com>
2023-04-23 19:08:04 +0200

    4.6 restart

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,17 +3,17 @@ import { shallow } from "enzyme";
 import NumberOfEvents from "../NumberOfEvents";
 
 describe("<NumberOfEvents /> component", () => {
-  let NumberOfEventsWrapper;
-  beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
-  });
+	let NumberOfEventsWrapper;
+	beforeAll(() => {
+		NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+	});
 
-  test("render NumberOfEvents element", () => {
-    expect(NumberOfEventsWrapper.find(".NumberOfEvents")).toHaveLength(1);
-  });
+	test("render NumberOfEvents element", () => {
+		expect(NumberOfEventsWrapper.find(".NumberOfEvents")).toHaveLength(1);
+	});
 
-  test("render text input", () => {
-    expect(NumberOfEventsWrapper.find("input.number")).toHaveLength(1);
-    expect(NumberOfEventsWrapper.find("label")).toHaveLength(1);
-  });
+	test("render text input", () => {
+		expect(NumberOfEventsWrapper.find("input.number")).toHaveLength(1);
+		expect(NumberOfEventsWrapper.find("label")).toHaveLength(1);
+	});
 });
