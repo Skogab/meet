c3010aef37bc0d84cbc4a2ce23fc64371edaeecd
Skogab <mierau.matthias@googlemail.com>
2023-05-05 11:16:09 +0200

    4.10 setup

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import NumberOfEvents from "./NumberOfEvents";
 import { OfflineAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
 class App extends Component {
 	state = {
@@ -18,6 +19,17 @@ class App extends Component {
 		selectedLocation: "all",
 		offlineText: "",
 		showWelcomeScreen: undefined,
+	};
+
+	// total number of events happening in each city, you need to count how many events each city has
+	getData = () => {
+		const { locations, events } = this.state;
+		const data = locations.map((location) => {
+			const number = events.filter((event) => event.location === location).length;
+			const city = location.split(", ").shift();
+			return { city, number };
+		});
+		return data;
 	};
 
 	async componentDidMount() {
@@ -72,8 +84,19 @@ class App extends Component {
 
 		return (
 			<div className="App">
+				<h1>Welcome to the City App</h1>
+				<h4>Choose a city</h4>
 				<CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
 				<NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+				<ResponsiveContainer height={400}>
+					<ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+						<CartesianGrid />
+						<XAxis type="category" dataKey="city" name="city" />
+						<YAxis allowDecimals={false} type="number" dataKey="number" name="number of events" />
+						<Tooltip cursor={{ strokeDasharray: "3 3" }} />
+						<Scatter data={this.getData()} fill="#8884d8" />
+					</ScatterChart>
+				</ResponsiveContainer>
 				<EventList events={this.state.events} />
 				{!navigator.onLine ? (
 					<OfflineAlert text={"You are offline, the displayed list has been loaded from the cache"} />
